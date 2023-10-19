104be7044a1a59a0dafbf991c07908cde77cd38e
KingsMunene <munenekings2@gmail.com>
2023-10-19 12:08:12 +0300

    Created an entity for our app. The entity is a class of what a goal contains

---
+++ app/src/main/java/com/example/goalsapp/data/Goals.kt
@@ -0,0 +1,12 @@
+package com.example.goalsapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Goal(
+    val title: String,
+    val description: String,
+    val isDone: Boolean,
+    @PrimaryKey val id: Int? = null
+)
