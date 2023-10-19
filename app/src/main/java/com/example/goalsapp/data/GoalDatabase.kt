c531a83b3dd3d589f486cf2a8610116e3317d57e
KingsMunene <munenekings2@gmail.com>
2023-10-19 12:30:29 +0300

    Minor comments

--- app/src/main/java/com/example/goalsapp/data/GoalDatabase.kt
+++ app/src/main/java/com/example/goalsapp/data/GoalDatabase.kt
@@ -5,10 +5,13 @@ import androidx.room.RoomDatabase
 
 
 // The instance of the RoomDatabase that has the instance of the GoalDao
+// We define the entities of the database in an Array
 @Database(
     entities = [Goal::class],
     version = 1
 )
+// The database is an abstract class that extends the RoomDatabase
+// An abstract class cannot be instantiated
 abstract class GoalDatabase: RoomDatabase() {
 
     abstract val dao : GoalDao
