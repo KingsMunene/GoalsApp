7de29c57698abe1b0fd8e4fd555d41a517b52961
KingsMunene <munenekings2@gmail.com>
2023-11-08 16:07:29 +0300

    Navigation for the app

--- app/src/main/java/com/example/goalsapp/MainActivity.kt
+++ app/src/main/java/com/example/goalsapp/MainActivity.kt
@@ -10,8 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.goalsapp.addEditGoal.AddEditGoalScreen
+import com.example.goalsapp.goalsList.GoalListScreen
 import com.example.goalsapp.ui.theme.GoalsAppTheme
+import com.example.goalsapp.utils.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +32,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = Routes.GoalList.name){
+                        composable(route = Routes.GoalList.name) {
+                            GoalListScreen(
+                                navigate = { navController.navigate(it.route) }
+                            )
+                        }
+
+                        composable(
+                            route = Routes.AddEditGoalItem.name + "?goalId={goalId}",
+                            arguments =listOf(
+                               navArgument(name = "goalId"){
+                                   type = NavType.IntType
+                                   defaultValue = -1
+                               }
+                            )
+                        ){
+                            AddEditGoalScreen(popBackStack = { navController.popBackStack() })
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GoalsAppTheme {
-        Greeting("Android")
+
     }
 }
