be4b4219bf229499738d604f9a3cb059fa113ade
KingsMunene <munenekings2@gmail.com>
2023-11-08 16:07:55 +0300

    make actionLabel nullable

--- app/src/main/java/com/example/goalsapp/utils/UiEvents.kt
+++ app/src/main/java/com/example/goalsapp/utils/UiEvents.kt
@@ -3,7 +3,7 @@ package com.example.goalsapp.utils
 sealed class UiEvents{
     data class ShowSnackBar(
         val message: String,
-        val actionLabel: String
+        val actionLabel: String?
     ): UiEvents()
 
     data class Navigate(val route: String): UiEvents()
